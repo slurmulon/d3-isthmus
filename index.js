71f6fc7b436a48c8d1d72288f8e55a4a08912afe
evavro <me@madhax.io>
2016-10-23 21:42:12 -0700

    :seedling: initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+export {
+  event,
+  select,
+  selectAll
+} from 'd3-selection'
+
+export * from 'd3-selection'
+
+// import 'd3-axis'
+// import 'd3-scale'
+// import 'd3-drag'
+
+export * from 'd3-axis'
+export * from 'd3-scale'
+export * from 'd3-drag'
+
+// export {
+//   axis
+// } from 'd3-axis'
+
+// export {
+//   scale
+// } from 'd3-scale'
+
+// export {
+//   drag
+// } from 'd3-drag'
